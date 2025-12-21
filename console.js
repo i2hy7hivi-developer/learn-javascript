66738050547c5ec092603964fce9f525596c68e5
Rhythm Dubey <125437535+i2hy7hivi-developer@users.noreply.github.com>
2025-12-21 15:23:43 +0000

    Console Statements

---
+++ console.js
@@ -0,0 +1,32 @@
+// console.log('Hello Rhythm!');
+
+// console.error('Hello Rhythm!');
+
+// console.warn('Hello Rhythm!');
+
+// console.info('Hello Rhythm!');
+
+// console.debug('Hello Rhythm!');
+
+// console.table([[1, 2, 3, 4, 5, 6], [1, 2, 3, 4, 5, 6], [1, 2, 3, 4, 5, 6], [1, 2, 3, 4, 5, 6], [1, 2, 3, 4, 5, 6], [1, 2, 3, 4, 5, 6]])
+
+// console.clear()
+
+// console.dir('rhythm')
+
+// outputs a stack trace to the console, showing the path your code took to reach the point where it was called. This helps track the flow of execution and identify where a function was invoked.
+// function a() {
+//     b()
+// }
+// function b() {
+//     c()
+// }
+// function c() {
+//     console.trace()
+// }
+// a()
+
+// console.count('rhythm')
+// console.count('rhythm')
+// console.count('rhythm')
+// console.count('rhythm')
