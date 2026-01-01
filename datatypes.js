b64134f852c51b2c306521fdc953d95c83971d67
Rhythm Dubey <125437535+i2hy7hivi-developer@users.noreply.github.com>
2026-01-01 23:09:00 +0000

    DataTypes

---
+++ datatypes.js
@@ -0,0 +1,25 @@
+"use strict" // Treat all JS code as newer version
+
+// alert("hello") // Works inside document (browser)
+
+// Code readability
+
+let name = "Rhythm"
+let age = 34
+let isLoggedIn = true
+
+// // Primitive
+// Number => 2 to 2 power 53
+// BigInt
+// String
+// Boolean
+// Null - Standalone Value
+// Undefined
+// Symbol - Unique
+
+
+// Object
+
+// console.log(typeof age) // number
+console.log(typeof undefined) // undefined
+console.log(typeof null) // object
