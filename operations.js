499b5f3dab334b85ecccae639a0d3f55c85085f9
Rhythm Dubey <125437535+i2hy7hivi-developer@users.noreply.github.com>
2026-01-01 23:34:10 +0000

    Operations

---
+++ operations.js
@@ -0,0 +1,14 @@
+let value = 3
+let negativeValue = -value
+// console.log(negativeValue) // -3
+
+// console.log("1" + 2)
+// console.log(2 + "1")
+// console.log("1" + 2 + 2)
+// console.log(2 + 2 + "3")
+
+// ++value
+// console.log(value) // 4
+
+value++
+console.log(value) // 4
